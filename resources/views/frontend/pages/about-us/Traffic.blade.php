bbf4f3f3e40a6d896e02981cd5118f3be2095f67
MuhRezaAldiIrawan <titaniaelvs@gmail.com>
2023-02-24 16:01:57 +0800

    Traffic fiture filter

--- resources/views/frontend/pages/about-us/Traffic.blade.php
+++ resources/views/frontend/pages/about-us/Traffic.blade.php
@@ -2,45 +2,65 @@
 
 
 @section('content')
-    <main class="container">
-        <form method="POST" action="{{route('login')}}">
-            @csrf
-            <h4 class="mb-2 card-title">Filter Data</h4>
-            <div class="d-flex flex-direction-row">
-                <div>
-                    <select class="custom-select">
-                        <option selected>Open this select menu</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                    <div class="d-flex flex-row " style="gap: 20px; margin-top: 10px;">
-                        <select class="custom-select">
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-                        <select class="custom-select">
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
+    <div class="container flex-grow-1 container-p-y">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mb-4">
+                    <h5 class="card-header">Filtering Data</h5>
+                    <div class="card-body">
+                        <div>
+                            <label for="defaultFormControlInput" class="form-label">Location</label>
+                            <select class="custom-select">
+                                <option selected disabled>Pilih Lokasi Filter data</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="mb-3 col-md-6">
+                                <label for="lastName" class="form-label">Start Date</label>
+                                <input class="form-control" type="date" />
+                            </div>
+                            <div class="mb-3 col-md-6">
+                                <label for="lastName" class="form-label">End Date</label>
+                                <input class="form-control" type="date" />
+                            </div>
+                        </div>
+                        <button type="button" class="btn btn-primary">
+                            <span class="ft-search"></span>&nbsp; Search
+                          </button>
                     </div>
                 </div>
-                <div>
-                    <div id="date-picker-example" class="md-form md-outline input-with-post-icon datepicker" inline="true">
-                        <input placeholder="Select date" type="text" id="example" class="form-control">
-                        <label for="example">Try me...</label>
-                        <i class="fas fa-calendar input-prefix"></i>
-                      </div>
-                </div>
             </div>
-            <div class="d-flex justify-content-between flex-sm-row flex-column">
-                <a href="/admin/login" class="btn bg-light-primary mb-2 mb-sm-0">Admin</a>
-                <button type="submit" class="btn btn-primary">Masuk</button>
+        </div>
+    </div>
+
+
+
+    {{-- <div class="container flex-grow-1 container-p-y">
+    <div class="row">
+        <div class="col-md-12">
+          <div class="card mb-4">
+            <h4 class="mb-2 card-title">Filter Data</h4>
+            <div class="card-body">
+                <form method="POST" action="{{route('login')}}">
+                    @csrf
+                    
+                    <div class="d-flex flex-direction-row">
+                    
+                            <label for="defaultFormControlInput" class="form-label">Location</label>
+                            <select class="custom-select" aria-label="Default select example">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                              </select>
+                    </div>
+                </form>
             </div>
-        </form>
-    </main>
+          </div>
+        </div>
+    </div>
+</div> --}}
 @endsection
